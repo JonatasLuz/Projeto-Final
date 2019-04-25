042499299a4a7fd076df69f5a4c42df6b3c0ec64
Jonatas da Luz <jonatas.luz.91@gmail.com>
2019-04-24 21:56:54 -0300

    Adicionado nav bar

--- Projeto Final/View Controller/PlantsTableViewController.swift
+++ Projeto Final/View Controller/PlantsTableViewController.swift
@@ -30,7 +30,7 @@ class PlantsTableViewController: UITableViewController, UITextFieldDelegate, UII
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 25
+        return 20
     }
 
    
